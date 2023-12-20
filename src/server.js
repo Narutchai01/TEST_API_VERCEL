d5c076fcf1076322bd410a77d619d97301816bf7
Narutchai <koonfluk2015@gmail.com>
2023-12-21 05:11:25 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const port = process.env.PORT || 3000;
+
 
 const app = express();
 app.use(cors());
@@ -8,4 +10,9 @@ app.use(cors());
 
 app.get('/api', (req, res) => {
     res.send('Hello World!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
